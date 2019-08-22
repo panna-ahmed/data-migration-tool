6cccfb6c99abd13d29098aeb05ac33d6c4a09b0e
Saleh Ahmed Panna <panna.ahmed@outlook.com>
2019-08-22 17:35:11 +0600

    added

---
+++ DataMigrationTool/Table.cs
@@ -0,0 +1,25 @@
+﻿namespace DataMigrationTool
+{
+    public class Table
+    {
+        public string Name { get; set; }
+        public string SQLName {
+            get {
+                return "[" + Name + "]";
+            }
+        }
+        public string Schema { get; set; } = "dbo";
+        public bool Target { get; set; } = false;
+        public string FullName
+        {
+            get
+            {
+                if(Target == false)
+                    return Schema + ".[#" + Name + "]";
+                else
+                    return Schema + ".[" + Name + "]";
+            }
+        }
+
+    }
+}
